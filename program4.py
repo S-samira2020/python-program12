7e477ef7a3418dcb130bed88486e6c555b973f20
Samira <54632872+S-samira2020@users.noreply.github.com>
2020-09-26 23:20:02 +0600

    Add files via upload

---
+++ program4.py
@@ -0,0 +1,11 @@
+# 5.1
+
+car  = 'subaru'
+print(" Is car == 'subaru'? I pretic true.")
+print(car == 'subaru')
+print()
+print("Is car == 'audi'? i predic false.")
+print(car == 'audi')
+print()
+
+
---
+++ program5.py
@@ -0,0 +1,37 @@
+# 5.2
+
+name  = 'samira'
+print("no-1")
+print(name  == 'samira')
+print()
+print("no-2")
+print(name  == 'rifat')
+print()
+name  = 'sakiba'
+print("no-3")
+print(name == 'rifat')
+print()
+print("no-4")
+print(name  == 'sakiba')
+print()
+name  = 'belayet'
+print("no-5")
+print(name == 'belayet')
+print()
+print("no-6")
+print(name == 'rafee')
+print()
+name  = 'rafee'
+print("no-7")
+print(name  == 'rafee')
+print()
+print("no-8")
+print(name  == 'rifat')
+print()
+name  = 'rifat'
+print("no-9")
+print(name  == 'rifat')
+print()
+print("no-10")
+print(name == 'rafee')
+print()
